b61cad3b4d07cb0187675d233d5360bc40db166f
gusinpng <bagusindra153@gmail.com>
2026-06-09 18:20:47 +0700

    Update Pembayaran.java

--- Pembayaran.java
+++ Pembayaran.java
@@ -1,28 +1,36 @@
 public class Pembayaran {
 
+    private Pemesanan pemesanan;
     private boolean status;
 
-    public Pembayaran() {
-        status = false;
+    public Pembayaran(Pemesanan pemesanan) {
+        this.pemesanan = pemesanan;
+        this.status = false;
     }
 
-    public void bayar(String rekening, int nominal, double hargaPaket)
-        throws InvalidPaymentException {
+    public void bayar(String rekening, int nominal)
+            throws InvalidPaymentException {
 
-    if (!rekening.equals("12345678")) {
-        throw new InvalidPaymentException(
-                "Harap memasukkan nomor rekening yang benar!");
+        double hargaPaket = pemesanan.getPaket().getHarga();
+
+        if (!rekening.equals(NOMOR_REKENING)) {
+            throw new InvalidPaymentException(
+                    "Harap memasukkan nomor rekening yang benar!");
+        }
+
+        if (nominal < hargaPaket) {
+            throw new InvalidPaymentException(
+                    "Nominal pembayaran kurang.");
+        }
+
+        status = true;
     }
-
-    if (nominal < hargaPaket) {
-        throw new InvalidPaymentException(
-                "Nominal pembayaran kurang.");
-    }
-
-    status = true;
-}
 
     public boolean isStatus() {
         return status;
+    }
+
+    public Pemesanan getPemesanan() {
+        return pemesanan;
     }
 }
